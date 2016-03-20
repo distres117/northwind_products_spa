a4c1090700a6ecb00e69499e1b75ef6abee50d9d
distres117 <omcrobbie@gmail.com>
2016-03-20 13:08:19 -0400

    deployment

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var conn;
 module.exports = function(){
   if (!conn){
-    conn = mongoose.connect('mongodb://localhost/products-spa', function(err){
+    conn = mongoose.connect( process.env.MONGOLAB_URI || 'mongodb://localhost/products-spa', function(err){
       if (err)
         console.log(err);
     });
