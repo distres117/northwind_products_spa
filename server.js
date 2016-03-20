c3f930d9b9bb7f81cac0d936f9f43a4853a6f922
distres117 <omcrobbie@gmail.com>
2016-03-20 13:28:49 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = require('./app'),
 dbconnect()
 .then(function(){
   console.log('db is connected...');
-  app.listen(process.env.port || 3000, function(){
+  app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running...");
   });
 });
